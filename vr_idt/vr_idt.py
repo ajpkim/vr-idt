d546741b3432ea6cd68c17beed34ef0c18a4e370
Alex Kim <alex@ajpkim.com>
2022-02-03 13:34:17 -0500

    Further refactor of vr_idt.py
    Minor edits to setup.cfg and updating to standard Python .gitignore.

--- vr_idt/vr_idt.py
+++ vr_idt/vr_idt.py
@@ -1,37 +1,26 @@
-import argparse
-import math
-import os
-
-from datetime import datetime
-from pathlib import Path
 from typing import List
 
 import numpy as np
 import pandas as pd
-# Suppress pandas warning that we can safely ignore
-# pd.options.mode.chained_assignment = None
-
-"""
-In the process of refactoring this.
-"""
 
 GAZE_COLS = ["gaze_world_z", "gaze_world_y", "gaze_world_z"]
 HEAD_COLS = ["head_pos_z", "head_pos_y", "head_pos_z"]
 
+def foo():
+    print("HAHA")
 
-def get_gaze_head_matrices(df, cols):
-    """TODO"""
+def get_gaze_head_matrices(df: pd.DataFrame, cols: dict) -> (np.array, np.array):
+    """Return matrices with 3d vectors for VR world gaze locations and head positions."""
     gaze_cols = [cols[x] for x in GAZE_COLS]
     head_cols = [cols[x] for x in HEAD_COLS]
 
-    gaze_matrix = df.loc[:, [gaze_cols]].values
-    head_matrix = df.loc[:, [head_cols]].values
+    gaze_coords = df.loc[:, [gaze_cols]].values
+    head_coords = df.loc[:, [head_cols]].values
 
-    return gaze_matrix, head_matrix
+    return gaze_coords, head_coords
 
 
-
-def frequencies(df, time="time") -> pd.Series:
+def frequencies(df: pd.DataFrame, time="time") -> pd.Series:
     """Compute the sampling frequency for all data points based on
     adjacent sample times.
 
@@ -45,6 +34,11 @@ def frequencies(df, time="time") -> pd.Series:
     sample_freq = df["time"].diff()
     sample_freq[0] = df["time"].iloc[0]
     return sample_freq
+
+
+def valid_frequences(sample_freqs: np.array, min_freq: float) -> bool:
+    return any(freq < min_freq for freq in sample_freqs)
+
 
 def angle_between(v1: np.array, v2: np.array) -> float:
     """Compute the angle theta between vectors v1 and v2.
@@ -69,11 +63,16 @@ def angle_between(v1: np.array, v2: np.array) -> float:
     return np.rad2deg(theta)
 
 
-def valid_window_angles(gaze_world_matrix, head_pos_matrix,  max_angle):
-    """Check whether the dispersion angles within the given window data
-    are within the valid fixation threshold defined by max_angle.
+def valid_window_angles(
+        window_gaze_coords: np.array,
+        window_head_coords: np.array,
+        max_angle: int
+) -> bool:
+    """Return a boolean for whether the dispersion angles for all
+    pairs of samples in the given window are within the valid fixation
+    threshold defined by max_angle.
     """
-    vectors = gaze_world_matrix - np.mean(head_pos_matrix, axis=0)
+    vectors = window_gaze_coords - np.mean(window_head_coords, axis=0)
     for i in range(vectors.shape[0]):
         v1 = vectors[i]
         for j in range(i+1, vectors.shape[0]):
@@ -82,44 +81,46 @@ def valid_window_angles(gaze_world_matrix, head_pos_matrix,  max_angle):
                 return False
     return True
 
-def valid_frequences(hz: np.array, min_freq: float) -> bool:
-    """TODO"""
-    return any(freq < min_freq for freq in hz]
 
+def is_fixation(
+        gaze_coords: np.array,
+        head_coords: np.array,
+        sample_freqs: np.array,
+        window_start: int,
+        window_end: int,
+        max_angle: float,
+        min_freq: int
+) -> bool:
+    """Return a bool indicating whether the given window is part of a fixation"""
+    window_gaze_coords = gaze_coords[window_start:window_end+1]
+    window_head_coords = head_coords[window_start:window_end+1]
 
-def fixating(df,
-             window_start,
-             window_end,
-             dispersion_threshold=1.5,
-             min_freq=30) -> bool:
-    """Returns a bool indicating whether the given window is part of a fixation"""
-    window_data = df.iloc[window_start:window_end+1]
-    if not all(hz > min_freq for hz in window_data['hz']):
+    if not valid_frequences(sample_freqs[window_start:window_end+1], min_freq):
         return False
 
-    return check_window_dispersions(window_data, dispersion_threshold)
+    return valid_window_angles(window_gaze_coords, window_head_coords, max_angle)
 
 
-def vr_itd(df: pd.DataFrame,
-           min_window_time      =0.15,
-           dispersion_threshold =1.50,
-           min_freq             =30,
-           time                 ="time",
-           gaze_world_x         ="head_pos_z",
-           gaze_world_y         ="head_pos_z",
-           gaze_world_z         ="head_pos_z",
-           head_pos_x           ="head_pos_z",
-           head_pos_y           ="head_pos_z",
-           head_pos_z           ="head_pos_z",
-           inplace              =False,
-           ) -> pd.DataFrame:
+def vr_itd(
+        df: pd.DataFrame,
+        min_duration         =0.15,
+        max_angle            =1.50,
+        min_freq             =30,
+        time                 ="time",
+        gaze_world_x         ="gaze_world_x",
+        gaze_world_y         ="gaze_world_y",
+        gaze_world_z         ="gaze_world_z",
+        head_pos_x           ="head_pos_x",
+        head_pos_y           ="head_pos_y",
+        head_pos_z           ="head_pos_z",
+        inplace              =False
+) -> pd.DataFrame:
     """
     Implements the VR IDT algorithm as proposed in:
     https://www.mdpi.com/1424-8220/20/17/4956. Original code from
     authors is here:
     https://github.com/ASAPLableni/VR-centred_I-DT_algorithm.
     """
-
     # Mapping to column names in the given df
     cols = {"time": time,
             "gaze_world_x": gaze_world_x,
@@ -129,13 +130,12 @@ def vr_itd(df: pd.DataFrame,
             "head_pos_y": head_pos_y,
             "head_pos_z": head_pos_z}
 
-    df_cols = list(cols.values())
-    if not all(col in df.columns for col in df_cols):
+    if not all(col in df.columns for col in list(cols.values())):
         raise Exception(f"DataFrame is missing some columns from <{df_cols}>")
 
     # Initialize matrices, results DF, window indices
-    hz = frequencies(df[time])
-    gaze_matrix, head_matrix = get_gaze_head_matrices(df, cols)
+    sample_freqs = frequencies(df[time])
+    gaze_coords, head_coords = get_gaze_head_matrices(df, cols)
     fixation_cols = ["fixation", "fixation_start", "fixation_end", "fixation_duration"]
     fixation_df = pd.DataFrame(np.zeros(df.shape[0], len(fixation_cols), int),
                                columns=fixation_cols)
@@ -146,22 +146,21 @@ def vr_itd(df: pd.DataFrame,
     while window_start < final:
         window_end = window_start + 1
         # Extend window until total window time exceeds the given minimum valid window time
-        while (df[time].iloc[window_end] - df[time].iloc[window_start]) < min_window_time:
+        while (df[time].iloc[window_end] - df[time].iloc[window_start]) < min_duration:
             window_end += 1
             if window_end > final:
                 return fixation_df
 
         # Current window isn't a valid fixation, increment start
-        if (not valid_frequences(hz[window_start:window_end+1])
-            or not is_fixating(gaze_matrix, head_matrix, window_start, window_end)):
+        if not is_fixation(gaze_coords, head_coords, sample_freqs, window_start, window_end, max_angle, min_freq):
             window_start += 1
         else:
             # Extend the window while in a valid fixation
-            while (is_fixating(gaze_matrix, head_matrix, window_start, window_end)
+            while (is_fixation(gaze_coords, head_coords, sample_freqs, window_start, window_end, max_angle, min_freq)
                    and window_end <= final):
                 window_end += 1
             window_end -= 1  # decrement since we've exceeded the fixation window in last loop iteration
-            # Process the fixation window
+            # Process the previous fixation window
             duration = df[time].iloc[window_end] - df[time].iloc[window_start]
             fixation_df["fixation_start"].iloc[window_start] = 1
             fixation_df["fixation_end"].iloc[window_end] = 1
@@ -171,42 +170,3 @@ def vr_itd(df: pd.DataFrame,
             window_start = window_end
 
     return fixation_df
-
-
-if __name__ == '__main__':
-
-    # Prepare paths and directories
-    data_dir = Path(args.data_dir)
-    output_dir = Path(args.output_dir)
-
-    if not data_dir.exists():
-        raise Exception("Data directory doesn't exist:", data_dir)
-    if not output_dir.exists():
-        raise Exception("Output directory doesn't exist:", output_dir)
-
-    result_dir = f"disp_{args.angle_dispersion_threshold}_time_{args.min_window_time}_freq_{args.min_frequency}"
-    result_dir = result_dir.replace('.', '_')
-
-    print('##################################################')
-    print('################ Running VR IDT ##################')
-    print('##################################################')
-
-    try:
-        os.mkdir(output_dir / result_dir)
-    except FileExistsError:
-        print('Output directory already exists. May overwrite files.')
-    result_dir = output_dir / result_dir
-    print('Writing to:', result_dir)
-
-    # Process all the data files in given directory
-    for file in os.listdir(data_dir):
-        if file.endswith('.csv'):
-            print(f"{file}   ::   {datetime.now() - start}")
-            # Run classification algo and add fixation labels
-            df = vr_itd(data_dir / file,
-                        args.min_window_time,
-                        args.angle_dispersion_threshold,
-                        args.min_frequency)
-            df.to_csv(result_dir / file, index=False)
-
-    print('\nDONE')
